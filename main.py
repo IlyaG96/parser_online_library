22efab5bf34c8f4d5b61202bc237e119c7cc9133
IlyaG96 <ilyag_96@icloud.com>
2021-12-27 22:35:48 +1100

    safe download books with authors and titles

--- main.py
+++ main.py
@@ -2,44 +2,43 @@ import requests
 import pathlib
 from environs import Env
 from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
 
 
-def download_books(book_path):
+def parse_books_description(book_id, url):
 
-    for book_id in range(1, 11):
-        url = f"http://tululu.org/txt.php?id={book_id}"
-        response = requests.get(url, verify=False, allow_redirects=False)
-        response.raise_for_status()
-        if response.is_redirect:
-            continue
-        book = response.content
-        pathlib.Path(book_path).mkdir(parents=True, exist_ok=True)
+    address = f"{url}/b{book_id}/"
+    response = requests.get(address, verify=False, allow_redirects=False)
+    response.raise_for_status()
 
-        with open(file=f"./books/{book_id}.txt", mode="wb") as file:
-            file.write(book)
+    if response.is_redirect:
+        raise requests.HTTPError
 
+    content = BeautifulSoup(response.text, 'lxml')
+    header = content.find("div", {"id": "content"}).find('h1').text
+    book_title = 0
+    book_author = 1
+    title = header.split("::")[book_title].strip()
+    author = header.split("::")[book_author].strip()
 
-def parse_books_titles():
-    for book_id in range(1, 11):
-        url = f"http://tululu.org/b{book_id}/"
-        response = requests.get(url, verify=False, allow_redirects=False)
-        response.raise_for_status()
-
-        if response.is_redirect:
-            continue
-
-        content = BeautifulSoup(response.text, 'lxml')
-        header = content.find("div", {"id": "content"}).find('h1').text
-        book_title = 0
-        book_author = 1
-        title = header.split("::")[book_title].strip()
-        author = header.split("::")[book_author].strip()
+    return f"'{title}'-{author}"
 
 
-def download_txt(url):
+def download_txt(url, filename, book_path, book_id):
+    filename = sanitize_filename(filename)
+
+    address = f"{url}/txt.php?id={book_id}"
+    response = requests.get(address, verify=False, allow_redirects=False)
+    response.raise_for_status()
+    if response.is_redirect:
+        raise requests.HTTPError
+    book = response.content
+    pathlib.Path(book_path).mkdir(parents=True, exist_ok=True)
+
+    with open(file=f"./books/{filename}.txt", mode="wb") as file:
+        file.write(book)
 
 
-    pass
 
 def parse_books_imgs():
     pass
@@ -49,10 +48,15 @@ def main():
     env = Env()
     env.read_env()
     book_path = env.str("book_path")
+    url = "http://tululu.org"
 
- #   download_books(book_path)
+    for book_id in range(1, 11):
+        try:
+            filename = parse_books_description(book_id, url)
+            download_txt(url, filename, book_path, book_id)
+        except requests.HTTPError:
+            continue
 
-    parse_books_titles()
 
 if __name__ == '__main__':
     main()
