a8cb47fad082aad5852bea2420460448555ca591
IlyaG96 <ilyag_96@icloud.com>
2021-12-27 19:45:33 +1100

    download 10 books from online library

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import requests
+import pathlib
+from environs import Env
 
 
-def download_book():
-    url = "http://tululu.org/txt.php?id=32168"
-    response = requests.get(url, verify=False)
-    book = response.content
 
-    with open(file="./books/file.txt", mode="wb") as file:
-        file.write(book)
+def download_books(book_path):
+
+    for id in range(10):
+        url = f"http://tululu.org/txt.php?id={id}"
+        response = requests.get(url, verify=False)
+        book = response.content
+        pathlib.Path(book_path).mkdir(parents=True, exist_ok=True)
+
+        with open(file=f"./books/{id}.txt", mode="wb") as file:
+            file.write(book)
 
 
-download_book()
+def main():
+    env = Env()
+    env.read_env()
+    book_path = env.str("book_path")
+
+    download_books(book_path)
