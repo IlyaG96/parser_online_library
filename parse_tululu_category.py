2e842a569781894f85a65243dbe1468854638d91
IlyaG96 <ilyag_96@icloud.com>
2022-01-15 20:14:32 +1100

    add argparser

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,18 +1,13 @@
-from pathvalidate import sanitize_filename
 from pathlib import Path
 from bs4 import BeautifulSoup
 from environs import Env
-from lxml import html
-from pprint import pprint
-from tqdm import tqdm
 import argparse
 import requests
-import os
 from main import download_txt, download_cover, collect_book_info, get_book_page_content
 import json
 
 
-def write_book_info_to_json(book_info):
+def write_book_info_to_json(book_info, json_path):
     with open(file="books.json", mode="a") as file:
         json.dump(book_info, file, ensure_ascii=False, indent=2)
 
@@ -34,21 +29,23 @@ def get_books_ids(page_content):
     return book_links
 
 
-def download_txt_and_cover(book_id, book_info, covers_path, book_path):
+def download_txt_and_cover(book_id, book_info, covers_path, book_path, args):
     title = book_info["title"]
     cover_link = book_info["cover_link"]
-    download_txt(title, book_path, book_id)
-    download_cover(cover_link, covers_path)
+    if not args.skip_imgs:
+        download_cover(cover_link, covers_path)
+    if not args.skip_txt:
+        download_txt(title, book_path, book_id)
 
 
-def download_book_pack(book_path, covers_path, books_ids):
+def start_parsing(book_path, covers_path, books_ids, args, json_path):
 
     for book_id in books_ids:
         try:
             book_page_content = get_book_page_content(book_id)
             book_info = collect_book_info(book_page_content)
-            download_txt_and_cover(book_id, book_info, covers_path, book_path)
-            write_book_info_to_json(book_info)
+            download_txt_and_cover(book_id, book_info, covers_path, book_path, args)
+            write_book_info_to_json(book_info, json_path)
         except requests.exceptions.HTTPError:
             continue
 
@@ -59,15 +56,56 @@ def main():
     env.read_env()
     book_path = env.str("book_path", default="./books")
     covers_path = env.str("covers_path", default="./covers")
+    json_path = env.str("json_path", defaul="./json")
 
-    Path(book_path).mkdir(parents=True, exist_ok=True)
-    Path(covers_path).mkdir(parents=True, exist_ok=True)
+    parser = argparse.ArgumentParser(description="Введите id книг (начальный и конечный)")
+    parser.add_argument("start_page",
+                        help="Номер первой страницы",
+                        type=int,
+                        default=1,
+                        nargs="?")
+    parser.add_argument("stop_page",
+                        help="Номер последней страницы",
+                        type=int,
+                        default=3,
+                        nargs="?")
+    parser.add_argument('-book_path',
+                        help='Вручную определить директорию для скачивания книг',
+                        default=book_path,
+                        type=str,
+                        nargs="?")
+    parser.add_argument('-skip_imgs',
+                        action="store_true",
+                        help='не загружать картинки',
+                        default=False)
+    parser.add_argument('-skip_txt',
+                        action="store_true",
+                        help='не загружить txt книг',
+                        default=False)
+    parser.add_argument('-json_path',
+                        help='вручную определить путь к *.json файлу',
+                        default=json_path,
+                        type=str,
+                        nargs="?")
+    parser.add_argument('-covers_path',
+                        help='вручную определить путь к *.json файлу',
+                        default=covers_path,
+                        type=str,
+                        nargs="?")
 
-    pages = range(1, 2)
+    args = parser.parse_args()
+    print(args)
+
+    pages = range(args.start_page, args.stop_page)
+
+    Path(args.book_path).mkdir(parents=True, exist_ok=True)
+    Path(args.covers_path).mkdir(parents=True, exist_ok=True)
+    Path(args.json_path).mkdir(parents=True, exist_ok=True)
+
     for page in pages:
         page_content = get_content(page)
         books_ids = get_books_ids(page_content)
-        download_book_pack(book_path, covers_path, books_ids)
+        start_parsing(book_path, covers_path, books_ids, args, json_path)
 
 
 if __name__ == '__main__':
