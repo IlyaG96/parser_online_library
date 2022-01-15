2de9d443fdbf071277bb51215e2f32f0af676cbf
IlyaG96 <ilyag_96@icloud.com>
2022-01-15 17:00:11 +1100

    add possibility to change pages

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -10,14 +10,14 @@ import requests
 import os
 from urllib.parse import urljoin
 
-def get_page_content():
 
-    address = f"http://tululu.org/l55/"
+def get_page_content(page):
+    address = f"http://tululu.org/l55/{page}/"
     response = requests.get(address, verify=False, allow_redirects=False)
     response.raise_for_status()
 
-    if response.is_redirect:
-        raise requests.HTTPError
+ #   if response.is_redirect:
+ #       raise requests.HTTPError
 
     page_content = BeautifulSoup(response.text, "lxml")
 
@@ -25,8 +25,18 @@ def get_page_content():
 
 
 def get_book_links(page_content):
-
     links = page_content.select("#content>table>tr:nth-child(2)>td>a", href=True)
     base_url = "http://tululu.org/"
     book_links = [urljoin(base_url, str(link["href"])) for link in links]
     pprint(book_links)
+
+
+def main():
+    pages = range(1, 10)
+    for page in pages:
+        page_content = get_page_content(page)
+        get_book_links(page_content)
+
+
+if __name__ == '__main__':
+    main()
